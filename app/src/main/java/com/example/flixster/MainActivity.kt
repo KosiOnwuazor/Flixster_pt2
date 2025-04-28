029834ab16d8cf29862fa79d4e115a704136e216
brunoonwuazor <Brunoaustin07@gmail.com>
2025-04-28 11:57:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.flixster
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity(), OnListFragmentInteractionListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, MovieFragment())
+            .commit()
+    }
+
+    override fun onMovieSelected(movie: Movie) {
+        // Handle movie selection (e.g., show details or navigate to another screen)
+    }
+}
